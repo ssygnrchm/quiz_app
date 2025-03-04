2b914e7a15f47659f5b3a25ae54f9018f96d0480
Sisy Agnia <sisy.agniar10@gmail.com>
2025-03-04 13:49:57 +0700

    first commit

---
+++ lib/presentation/pages/login_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/presentation/pages/main_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        alignment: Alignment.center,
+        // height: MediaQuery.sizeOf(context).height,
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/login_screen.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            sizedBox(height: MediaQuery.sizeOf(context).height / 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Image.asset("assets/images/circle_logo.png")],
+            ),
+            sizedBox(height: MediaQuery.sizeOf(context).height / 7),
+            Column(
+              spacing: 10,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: text(
+                    "Enter your name",
+                    fsize: 18,
+                    fweight: FontWeight.w600,
+                  ),
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white, // White border color
+                        width: 1.0, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white, // White border color
+                        width: 1.0, // Border width
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    hintText: 'Enter text',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            sizedBox(height: MediaQuery.sizeOf(context).height / 5),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: MediaQuery.sizeOf(context).width,
+                minHeight: 59,
+                maxHeight: MediaQuery.sizeOf(context).width / 4,
+              ),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(
+                    const Color.fromARGB(255, 248, 198, 96),
+                  ),
+                  foregroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                  );
+                },
+                child: text("Start", fsize: 24, fweight: FontWeight.w600),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox sizedBox({double? height, double? width}) {
+    return SizedBox(height: height, width: width);
+  }
+
+  Text text(
+    String string, {
+    double fsize = 18,
+    FontWeight fweight = FontWeight.normal,
+    Color fcolor = Colors.white,
+  }) {
+    return Text(
+      string,
+      style: TextStyle(fontSize: fsize, fontWeight: fweight, color: fcolor),
+    );
+  }
+}
